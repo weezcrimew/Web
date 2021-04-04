b1a551605a12dc749d44072a358d0894d059d32c
weezcrimew <weezcrime@mail.ru>
2021-04-04 22:11:09 +0500

    add pvp mode

--- Lab5/cubes.js
+++ Lab5/cubes.js
@@ -4,68 +4,158 @@
       brosok : 0,
       schet : 0
     }
-    
+    var secondplayer = {
+      brosok : 0,
+      schet : 0
+    }
     var computer = {
       brosok : 0,
       schet : 0
     }
     let cubes = 1;
     
-    const dragZone = document.querySelector('#dragZone');
-    const cubeZone = document.querySelector('#cubeZone');
-    let cubeObj = document.querySelector('.cubeObj');
-    let pvp = document.getElementById("switch");
-    if (pvp.checked == true){
-      console.log("check");
-    } 
+    const dragZone = document.querySelector('.dragZone');
+    const cubeZone = document.querySelector('.cubeZone');
+    let pvpZone = document.querySelector('.pvpZone');
+    let cubeObj1 = document.querySelector('.cubeObj');
+    let pvp = document.getElementById("pvpCheck");
+    var cubeObj2 = document.createElement("img");
+    cubeObj2.src = "./img/cube1.jpg"
+    cubeObj2.className = "cubeObj"
+    cubeObj2.id = "cubObj2"
+
+    let pl1RollCounter = 0;
+    let pl2RollCounter = 0;
+
+    pvp.onclick = ()=>{
+      if (pvp.checked == true){
+        pvpZone.className = "cubeZone"
+        pvpZone.appendChild(
+          cubeObj2
+        )
+        $('.player-roll').removeClass('green')
+        $('.player-roll').removeClass('yellow')
+        $('.computer-roll').removeClass('green')
+        $('.computer-roll').removeClass('yellow')
+        $('.roll').text("");
+      }
+      else {
+        pvpZone.className = "pvpZone"
+        pvpZone.removeChild(cubeObj2);
+        $('.player-roll').removeClass('green')
+        $('.player-roll').removeClass('yellow')
+        $('.computer-roll').removeClass('green')
+        $('.computer-roll').removeClass('yellow')
+        $('.roll').text("");
+      } 
+    }
+
     dragZone.ondragover = allowDrop;
-    cubeZone.ondragover = allowDrop;
+
+   
 
     function allowDrop(event){
         event.preventDefault();
     }
 
-    cubeObj.ondragstart = drag;
+    cubeObj1.ondragstart = drag1;
+    cubeObj2.ondragstart = drag2;
 
-    function drag(event){
-        
-        event.dataTransfer.setData('id', event.target.id);
-        
+    function drag1(event){   
+        event.dataTransfer.setData('id', event.target.id); 
+        player.brosok = random(1, 6)
+        pl1RollCounter++
+         
     }
-
+    function drag2(event){   
+      event.dataTransfer.setData('id', event.target.id);    
+      secondplayer.brosok = random(1, 6)
+      pl2RollCounter++  
+    }
     dragZone.ondrop = drop;
-    cubeZone.ondrop = drop;
 
     function drop(event){
+
+      console.log(pl1RollCounter + " " +pl2RollCounter);
       $('.player-roll').removeClass('green')
       $('.player-roll').removeClass('yellow')
       $('.computer-roll').removeClass('green')
       $('.computer-roll').removeClass('yellow')
       $('.roll').text("");
       let itemId = event.dataTransfer.getData('id');
-      roll();
-     
+      pvp.checked == true ? rollPVP() : rollComp();
     }
 
-    function roll(){
-      player.brosok = random(1, 6)
-      document.getElementById("cubeObj").src="./img/cube"+player.brosok+
-      ".jpg";
+    function rollPVP(){
 
-      $('.player-roll').text(player.brosok)
-      
-      setTimeout(() => {
-        computer.brosok = random(1,6)
-        
+      $('.player-roll').removeClass('green')
+      $('.player-roll').removeClass('yellow')
+      $('.computer-roll').removeClass('green')
+      $('.computer-roll').removeClass('yellow')
 
-        $('.computer-img').addClass('rotate360')
+      if(player.brosok != 0){
+        document.getElementsByClassName("cubeObj").src="./img/cube"+player.brosok+
+        ".jpg";
+         $('.player-roll').text(player.brosok)
+      }
+     
+    if(secondplayer.brosok != 0){
+      $('.computer-img').addClass('rotate360')
+      $('.computer-roll').text(secondplayer.brosok)
+    }
+      if( pl1RollCounter ==  pl2RollCounter ){
+        if(player.brosok > secondplayer.brosok) {
+          player.schet += 1;
+          $('.player-roll').addClass('green')
+        } else if (player.brosok < secondplayer.brosok){
+          secondplayer.schet +=1;
+          $('.computer-roll').addClass('green')
+        } else {
+          $('.player-roll').addClass('yellow')
+          $('.computer-roll').addClass('yellow')
+        }
+        $('.player-total').text("Счет: " + player.schet)
+        $('.computer-total').text("Счет: " +secondplayer.schet)
     
+        $('.player-img').removeClass('rotate360')
+        setTimeout(()=>{
+          player.brosok = 0
+          secondplayer.brosok = 0
+          $('.roll').text("");
+          
+        $('.player-roll').removeClass('green')
+        $('.player-roll').removeClass('yellow')
+        $('.computer-roll').removeClass('green')
+        $('.computer-roll').removeClass('yellow')
+
+        }, 1000)
+        
+        if(player.schet == 6 || secondplayer.schet == 6){
+          if(player.schet > secondplayer.schet){
+            $('.winner').text('Вы победили!')
+          } else {
+            $('.winner').text('Победил компьютер!')
+          }
+          $('.push').prop('disabled', true)
+     }  
+    }
+    
+  }
+  
+    function rollComp(){
+      player.brosok = random(1, 6)
+      document.getElementsByClassName("cubeObj").src="./img/cube"+player.brosok+
+      ".jpg";
+      $('.player-roll').text(player.brosok)
+
+      computer.brosok = random(1,6)
+        
+        $('.computer-img').addClass('rotate360')
         $('.computer-roll').text(computer.brosok)
     
         if(player.brosok > computer.brosok) {
           player.schet += 1;
           $('.player-roll').addClass('green')
-    
         } else if (player.brosok < computer.brosok){
           computer.schet +=1;
           $('.computer-roll').addClass('green')
@@ -87,9 +177,6 @@
           }
           $('.push').prop('disabled', true)
         }
-      }, 1000)
-    
-    
     }
     
     $('.start').click(function(){
